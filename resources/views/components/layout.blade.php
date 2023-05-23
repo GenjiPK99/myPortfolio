bd4e1208a5993e37750ffa6f98bf462c5613df36
Domenico Caraviello <domenicocaraviello1999@gmail.com>
2023-05-23 19:21:51 +0200

    Building degli Assets terminato

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,8 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>myPortfolio</title>
+    @vite (['resources/css/app.css' , 'resources/js/app.js' ])
 </head>
 <body>
-    {{ $slot }}
+    <x-navbar/>
+    <main>
+        {{ $slot }}
+    </main>
+    <footer>
+        <x-footer/>
+    </footer>
 </body>
 </html>

69923f8ef005c69f2c16ee843087d128a6ac1981
Domenico Caraviello <domenicocaraviello1999@gmail.com>
2023-05-28 21:29:31 +0200

    Aggiunte nuove classi css + footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-<div class="intro-area">
+<div class=" intro-area header">
     <div class="container">
       <h2>Domenico Caraviello</h2>
       <p>Salutate il vostro amichevole Junior Dev di quartiere!</p>

5410d823d9f5f3076c0a8f345cdb5dc45d298946
Domenico Caraviello <domenicocaraviello1999@gmail.com>
2023-05-30 13:33:19 +0200

    aggiunta img profilo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,21 @@
 <x-layout>
 <div class=" intro-area header">
-    <div class="container">
-      <h2>Domenico Caraviello</h2>
-      <p>Salutate il vostro amichevole Junior Dev di quartiere!</p>
+    <div class="container-fluid">
+        <div class="row ">
+          <div class="col-12 col-md-6 ">
+            <div class="person">
+              <div class="container-custom">
+                <div class="container-inner">
+                <img class="circle" src= "{{ asset('images/Domenico.png') }}" alt="My Image"/>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-md-6">
+            <h2>Pleasure, <br> I'm Domenico Caraviello</h2>
+            <p>Salutate il vostro amichevole Junior Dev di quartiere!</p>
+          </div>
+        </div>
     </div>
 </div>
 </x-layout>
